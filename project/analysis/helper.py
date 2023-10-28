1fce9c017e28e1f8549f1932758bd741fc504553
millen11 <millen11@rpi.edu>
2023-10-27 23:17:51 -0400

    millen11-addingGSCPI
    Adde global supply pressure index

--- project/analysis/helper.py
+++ project/analysis/helper.py
@@ -3,6 +3,7 @@ This helper module is to create dataframes for each market index, i.e. S&P 500, 
 from January 1st, 2010 to Now (December 31st, 2023)
 
 Implementation comes from: https://towardsdatascience.com/analyzing-world-stock-indices-performance-in-python-610df6a578f
+Global Supply Chain Index: https://www.newyorkfed.org/research/policy/gscpi#/interactive
 '''
 
 #Imports
@@ -11,7 +12,7 @@ import pandas as pd
 import yfinance as yf
 
 
-def create_market_data(market_index:str) -> pd.DataFrame: #Helper function to create different dataframes for market indexes from 2009 to 2023
+def create_market_data(market_index: str, gscpi_index_df: pd.DataFrame) -> pd.DataFrame: #Helper function to create different dataframes for market indexes from 2009 to 2023
   market_index_df = []
   ticker_data = yf.Ticker(market_index)
   ticker_df = ticker_data.history(period = "1d", start = "2010-1-1", end = "2023-12-31")
@@ -22,7 +23,7 @@ def create_market_data(market_index:str) -> pd.DataFrame: #Helper function to cr
   market_index_df["Region"] = market_index_df.ticker.apply(lambda ticker: add_region(ticker))
   market_index_df = market_index_df.style.set_table_attributes("style='display:inline'").set_caption(market_index + " Data from January 2010 to December 2023")   #Giving the dataframe a title/name
   # market_index_df = market_index_df.reset_index()
-  
+
   return market_index_df
 
 def add_region(market_index: str) -> pd.DataFrame:
@@ -46,7 +47,7 @@ def add_date(market_index_df: pd.DataFrame) -> pd.DataFrame: #Add month and day 
     market_index_df["Month"] = dtobj.month
     market_index_df["Days_of_Week"] = dtobj.dayofweek
   return market_index_df
-    
+
 def adjust_close(market_index_df: pd.DataFrame) -> pd.DataFrame: #Update price to have a universal "currency"
   price_change = []
   for open_price in market_index_df["Open"]: #Go through all open and closing prices
@@ -57,7 +58,7 @@ def adjust_close(market_index_df: pd.DataFrame) -> pd.DataFrame: #Update price t
   return market_index_df
   
 def add_pollution_index(market_index_df: pd.DataFrame, pollution_index: pd.DataFrame) -> pd.DataFrame:
-  pass
+  
 
 def get_impactful_events() -> dict: #Function to find the most impactful events on Stock Market from 2020 to 2023
   high_impact_times = {}
@@ -68,3 +69,19 @@ def get_impactful_events() -> dict: #Function to find the most impactful events 
   high_impact_times["Russian_Ukranian_War"] = russia_ukraine
   high_impact_times["Israeli_Hamas_War"] = third_infitada
   return high_impact_times
+
+def clean_gscpi_index_df(gscpi_index_df: pd.DataFrame) -> pd.DataFrame: #Cleaning GSCPI index dataframe, removing nulls and prior CSV/XLS  gscpi_index_df = gscpi_index_df[["Date","GSCPI"]]
+  gscpi_index_df = gscpi_index_df.dropna()
+  updated_dates = []
+  gscpi_index_df["Date"] = gscpi_index_df["Date"].apply(lambda date: updated_dates.append(date) if (date[7:] >= 2010) else 1) #Gather all data ranging from 2010 to now
+  
+  #Convert month format (name) to number for consistency between dataframes
+  months = {"Jan": 01, "Feb": 02, "Mar": 03, "Apr": 04, "May": 05, "Jun": 06, "Jul": 07, "Aug": 08, "Sep": 09, "Oct": 10, "Nov": 11, "Dec": 12}
+  for date in updated_dates:
+    for month in months:
+      if (date[4:7] == month):
+        date[4:7] = str(months[month])
+  gscpi_index_df["Date"] = updated_dates
+  return gscpi_index_df
+
+def create_
